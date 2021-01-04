d58b2ffda1d8bdabd298994313be1dc931a3e47f
HugoPoletto34 <67022441+HugoPoletto34@users.noreply.github.com>
2021-01-03 22:00:16 -0300

    text de link

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import '../'
+
+export default function Game({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
---
+++ pages/sobre.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+const App = () => {
+    return (
+        <div><h4>hey Index</h4>
+        <Link href="/index.js">
+            <a>Ola</a>
+        </Link>
+        </div>
+    )
+}
+export default App;
